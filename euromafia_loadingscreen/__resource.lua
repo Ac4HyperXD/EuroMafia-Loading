c46a4c13f012647ef294ee5dd24bc7871625c8dc
Ac4HyperXD <84621501+Ac4HyperXD@users.noreply.github.com>
2021-06-14 10:08:35 +0200

    Add files via upload

---
+++ euromafia_loadingscreen/__resource.lua
@@ -0,0 +1,17 @@
+files {
+    'index.html',
+    'png.png',
+    'gif.gif',
+    'Discord.png',
+    'audio.mp3',
+    'video.js',
+    'css/style.css',
+    'css/index.css',
+    'js/music-controls.js',
+    'js/music-handler.js',
+    
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
